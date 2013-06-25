6fb9592a47dfe7821999b6e7300e866ed5da2edc
Vasyl Sloylo <vslotylo@gmail.com>
2013-06-25 18:18:20 +0300

    Added search files

---
+++ WebMarket/WebMarket/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using WebMarket.Core;
+using WebMarket.DAL.Entities;
+using WebMarket.Filters;
+
+namespace WebMarket.Controllers
+{
+    public class SearchController : ListControllerBase<Product>
+    {
+        public ActionResult Index(PageFilter pageFilter, SortFilter sortFilter, PageSizeFilter pageSizeFilter, SearchFilter searchFilter)
+        {
+            //var seperators = new[] { " ", "-" };
+            //var tokens = keyword.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+            //this.ViewModel = new FilterViewModelBase<Product>(pageSizeFilter, sortFilter, pageFilter, searchFilter);
+            //var entities = DbContext.Avrs.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
+            //this.StartInitializeCommon(entities.Count());
+            //this.EndInitialize(entities);
+            return this.View(this.ViewModel);
+        }
+    }
+}
