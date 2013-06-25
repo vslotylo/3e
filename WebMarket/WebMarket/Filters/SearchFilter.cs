5286f51e22213b7671429b7bc6ad199b401d5984
Vasyl Sloylo <vslotylo@gmail.com>
2013-06-25 18:06:09 +0300

    Added search

---
+++ WebMarket/WebMarket/Filters/SearchFilter.cs
@@ -0,0 +1,21 @@
+﻿namespace WebMarket.Filters
+{
+    public class SearchFilter : FilterBase
+    {
+        public SearchFilter()
+        {
+            this.Keyword = string.Empty;
+            this.DefaultValue = this.Keyword;
+            this.Key = "keyword";            
+        }
+
+        public string Keyword { get; set; }
+        public override string Value
+        {
+            get
+            {
+                return this.Keyword;
+            }
+        }            
+    }
+}
