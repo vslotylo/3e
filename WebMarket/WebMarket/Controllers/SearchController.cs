15d325e4b39e8bdd2d2fcbd388da6f223ca8bf75
Vasyl Sloylo <vslotylo@gmail.com>
2013-06-26 18:51:15 +0300

    Refactoring

--- WebMarket/WebMarket/Controllers/SearchController.cs
+++ WebMarket/WebMarket/Controllers/SearchController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web.Mvc;
 using WebMarket.Core;
 using WebMarket.DAL.Entities;
@@ -11,21 +13,24 @@ namespace WebMarket.Controllers
 {
     public class SearchController : ListControllerBase<Product>
     {
+        private Expression<Func<Product, bool>> expression;
+
         public ActionResult Index(PageFilter pageFilter, SortFilter sortFilter, PageSizeFilter pageSizeFilter, SearchFilter searchFilter)
         {
-            //var seperators = new[] { " ", "-" };
-            //var tokens = keyword.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+            var seperators = new[] { " ", "-" };
+            var tokens = searchFilter.Keyword.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
             this.ViewModel = new FilterViewModelBase<Product>(pageSizeFilter, sortFilter, pageFilter, searchFilter);
-            var avrs = DbContext.Avrs.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
-            var batteries = DbContext.Batteries.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
-            var ups = DbContext.Ups.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
-            var converters = DbContext.Converters.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
-            var chargers = DbContext.Chargers.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
-            var voltageRelays = DbContext.VoltageRelays.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
-            var currentRelays = DbContext.CurrentRelays.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
-            var temperatureRelays = DbContext.TemperatureRelays.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
-            var timeRelays = DbContext.TimeRelays.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
-            var voltmeters = DbContext.Voltmeters.Include(obj => obj.Producer).Where(obj => obj.Name.Contains(searchFilter.Keyword));
+            this.expression = obj=> tokens.All(t=> obj.Name.Contains(t));
+            var avrs = DbContext.Avrs.Include(obj => obj.Producer).Where(this.expression);
+            var batteries = DbContext.Batteries.Include(obj => obj.Producer).Where(this.expression);
+            var ups = DbContext.Ups.Include(obj => obj.Producer).Where(this.expression);
+            var converters = DbContext.Converters.Include(obj => obj.Producer).Where(this.expression);
+            var chargers = DbContext.Chargers.Include(obj => obj.Producer).Where(this.expression);
+            var voltageRelays = DbContext.VoltageRelays.Include(obj => obj.Producer).Where(this.expression);
+            var currentRelays = DbContext.CurrentRelays.Include(obj => obj.Producer).Where(this.expression);
+            var temperatureRelays = DbContext.TemperatureRelays.Include(obj => obj.Producer).Where(this.expression);
+            var timeRelays = DbContext.TimeRelays.Include(obj => obj.Producer).Where(this.expression);
+            var voltmeters = DbContext.Voltmeters.Include(obj => obj.Producer).Where(this.expression);
             
             var result = new List<Product>();
             result.AddRange(avrs);
